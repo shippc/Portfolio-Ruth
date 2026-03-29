4167042850876670a2022c5ee7d1a8a6fc950348
shippc <wilhanmax@hotmail.com>
2026-03-29 19:14:50 -0300

    Projects Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,23 @@ animate(chars2, {
 });
 
 /*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects-swiper', {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    speed: 600,
 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+});
 
 /*=============== WORK TABS ===============*/
 
